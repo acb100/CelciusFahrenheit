2c704fc51f4de0e2d38bb31eb12719ff73398270
Anders <113690119+acb100@users.noreply.github.com>
2023-03-22 10:44:38 +0100

    Compiled to jar file

--- src/main/java/com/example/celciusfahrenheit/converter.java
+++ src/main/java/com/example/celciusfahrenheit/converter.java
@@ -4,15 +4,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 public class converter {
 
+    public boolean isCelsius;
+    public boolean isFahrenheit;
+
     public double convertedNumber;
-
-    public void celsiusToFahrenheit(double input) {
-        convertedNumber = input * 1.8 + 32;
-    }
-
-    public void fahrenheitToCelsius(double input) {
-        convertedNumber = (input - 32) / 1.8;
-    }
 
     @PostMapping("/")
     public double getConvertedNumber() {
